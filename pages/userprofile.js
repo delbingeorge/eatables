ce5a014caaabbd2b73bf3e1090379f772b75a18a
Delbin George <imdelbingeorge@gmail.com>
2023-01-23 12:55:35 +0700

    again

--- pages/userprofile.js
+++ pages/userprofile.js
@@ -5,7 +5,72 @@ import Link from "next/link";
 import React from "react";
 import eatFavImg from "../pages/media/images/eat-fav.png";
 import userAvatar from "../pages/media/images/userAvatar.png";
-import Saved from "./Saved";
+
+const Saved = [
+    {
+        itemId: 1,
+        eatFav: true,
+        bgImg: "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        itemName: "Stick Steak",
+        stars: "4",
+    },
+    {
+        itemId: 2,
+        eatFav: true,
+        bgImg: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=781&q=80",
+        itemName: "Slices of Heaven",
+        stars: "3",
+    },
+    {
+        itemId: 3,
+        eatFav: true,
+        bgImg: "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=710&q=80",
+        itemName: "Classic Deviled Eggs",
+        stars: "3",
+    },
+    {
+        itemId: 4,
+        eatFav: true,
+        bgImg: "https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        itemName: "Fruity Surprise",
+        stars: "2",
+    },
+    {
+        itemId: 5,
+        eatFav: true,
+        bgImg: "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80",
+        itemName: "Classic Fruity Surprise",
+        stars: "2",
+    },
+    {
+        itemId: 6,
+        eatFav: true,
+        bgImg: "https://images.unsplash.com/photo-1529042410759-befb1204b468?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80",
+        itemName: "Spicy meatballs",
+        stars: "2",
+    },
+    {
+        itemId: 7,
+        eatFav: true,
+        bgImg: "https://images.unsplash.com/photo-1565299507177-b0ac66763828?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=722&q=80",
+        itemName: "Juicy Burger",
+        stars: "2",
+    },
+    {
+        itemId: 8,
+        eatFav: false,
+        bgImg: "https://images.unsplash.com/photo-1565299585323-38d6b0865b47?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=780&q=80",
+        itemName: "Tasty Tacos",
+        stars: "3",
+    },
+    {
+        itemId: 9,
+        eatFav: false,
+        bgImg: "https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80",
+        itemName: "Potato Pork Cuts",
+        stars: "4",
+    },
+];
 
 function UserProfile() {
     return (
